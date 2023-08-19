1005666764fb6d1efaaf71f76365f56a6b2cdeff
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-08-19 09:59:52 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TehtavaB7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pyydetään käyttäjältä x ja y arvot
+            Console.Write("Syötä x-arvo: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Syötä y-arvo: ");
+            int y = int.Parse(Console.ReadLine());
+
+            // Suoritetaan laskutoimitus x += y
+            x += y;
+
+            // Tulostetaan tulos
+            Console.WriteLine("x = " + x);
+        }
+    }
+}
+
+
